745ee8b72077d2d24ebc74fff960e7f155eee879
KANITA VERMA <vermakanita13@gmail.com>
2024-08-19 00:28:31 +0530

    Add files via upload

---
+++ d4.java
@@ -0,0 +1,14 @@
+public class d4 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=6;j++){
+                if((i==3&&(j==2||j==3))||(i==4&&(j==1||j==6))||(i==5&&(j==2||j==3||j==4||j==5))||(j==5&&i<=5)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
