1e8d4d649dae53f796fbd71c5ab2f9f76b5a5a11
KANITA VERMA <vermakanita13@gmail.com>
2024-08-21 22:15:16 +0530

    Add files via upload

---
+++ g7.java
@@ -0,0 +1,14 @@
+public class g7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=6;j++){
+                if((i==1&&j>=2)||(i==2&&j==1)||j==6||(i==3&&(j==3)||(i==5&&j==2)||(i==6&&j==4))){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
