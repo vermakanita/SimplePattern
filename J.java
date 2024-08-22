850ada2717f2836874803b9c8d01325aa04c048a
KANITA VERMA <vermakanita13@gmail.com>
2024-08-22 22:58:49 +0530

    Add files via upload

---
+++ J.java
@@ -0,0 +1,15 @@
+public class J {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j =1;j<=7;j++){
+                if(i==1||j==5||(i>2&&j==3)||(i==5&&j==4)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
