c71d98352f0995c88382075f38fa3c3d40591fcf
KANITA VERMA <vermakanita13@gmail.com>
2024-09-06 22:53:33 +0530

    Add files via upload

---
+++ SquareStarP.java
@@ -0,0 +1,17 @@
+public class SquareStarP
+{
+  public static void main(String[]arg)
+    {
+    int n = 4 ; 
+    for(int i=1; i<=n; i++)
+    {
+
+        for (int j=1; j<=n; j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println(" ");
+    }
+
+}
+}
